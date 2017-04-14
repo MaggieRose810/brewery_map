43e25a09bfa7e1601ebfe1ca284f26e77cf8aff1
Maggie Rose <maggie.rose.g@gmail.com>
2017-04-13 22:32:01 -0500

    Embed brewery in the locations api response

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::LocationsController < ApplicationController
   def index
-    @locations = Location.geocoded.limit(50)
-    render json: @locations
+    @locations = Location.geocoded.limit(50).includes(:brewery)
   end
 end
